2fc8be5676e3c314f506fbd5d72876787c6c6a56
Cameron Montgomery <jc.montgomery0102@gmail.com>
2023-05-10 09:51:32 -0600

    Create read and create actions and templates

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -12,9 +12,19 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new
   end
 
   def create
+    # Since the form is providing an array called subject due to using the helpers in new.html.erb we can access them as params[:subject]
+    @subject = Subject.new(params[:subject])
+    if @subject.save
+      redirect_to(subjects_path)
+    else
+      # Calling render.new this way will render the new template without running the code in the new action, instead using any values that exist in
+      # the create actions context. Here it doesn't matter but if the template relies on values from new we need to provide those or add the new above render
+      render('new')
+    end
   end
 
   def edit
