42a4f67a48d88beef0101a2e65358226bef27d49
Cameron Montgomery <jc.montgomery0102@gmail.com>
2023-05-17 16:42:15 -0600

    add new and edit actions, templates and form partial

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -17,7 +17,7 @@ class SubjectsController < ApplicationController
 
   def create
     # Since the form is providing an array called subject due to using the helpers in new.html.erb we can access them as params[:subject]
-    @subject = Subject.new(params[:subject])
+    @subject = Subject.new(subject_params) # Uses private method that requires and permits :subject keys
     if @subject.save
       redirect_to(subjects_path)
     else
@@ -28,9 +28,16 @@ class SubjectsController < ApplicationController
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
+    @subject = Subject.find(params[:id])
+    if @subject.update(subject_params)
+      redirect_to(subjects_path(@subject)) # Routes to the show action to see a detailed view
+    else
+      render('edit')
+    end
   end
 
   def delete
@@ -38,4 +45,11 @@ class SubjectsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def subject_params
+      # Mass assignment is not allowed in Rails by default unless we explicetly permit the certain parameters we wish to allow.
+      params.require(:subject).permit(:name, :position, :visible)
+    end
 end
